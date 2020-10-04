7555262411862ca0bd6472d9fe022e36b6ddf05b
agdsarge <agdsargeant@gmail.com>
2020-10-04 15:03:07 -0400

    final edits

--- Forecast.jsx
+++ Forecast.jsx
@@ -7,50 +7,14 @@ const refreshFrequency = 60 * 1000 * 60;
 
 const className = {
     ...fontInfo,
+    font: '15px Inconsolata',
     bottom: '1%',
-    right: '20%',
+    left: '35%',
     textAlign: 'left',
-    width: '620px'
+    whiteSpace: 'pre-wrap'
 }
 
-
-const column = css({
-    float: 'left',
-    width: '180px',
-    textAlign: 'left'
-})
-
-const row = css({
-    display: 'table',
-    clear: 'both',
-})
-
-const render = ({output}) => {
-    console.log(output)
-    let [now, w0, next, w1, later, w2] = output.split(',')
-    if (!!output) {
-        return (
-            <div>
-                <div className={row}>
-                    <div className={column} >
-                        <div> {now} </div>
-                        <div> {w0} </div>
-                    </div>
-                    <div className={column} >
-                        <div> {next} </div>
-                        <div> {w1} </div>
-                    </div>
-                    <div className={column} >
-                        <div> {later} </div>
-                        <div> {w2} </div>
-                    </div>
-                </div>
-            </div>
-            
-        )
-    } else {
-        return <div></div>
-    }
-}
+const render = ({output}) => <div>{output}</div>   
 
 export {command, refreshFrequency, className, render}
+   
--- forecast.sh
+++ forecast.sh
@@ -2,15 +2,11 @@ query=$(wget -qO- https://api.weather.gov/gridpoints/AKQ/72,67/forecast \
 | grep -E 'name|shortForecast' \
 | head -n6 \
 | sed 's/"name": //' \
-| sed 's/"shortForecast"//' \
+| sed 's/"shortForecast"://' \
 | xargs \
-| sed 's/[\:\"]//g' \
-| sed -e 's/^[[:space:]]*//' \
-| sed 's/\,$//')
+| sed 's/[\"]//g' \
+| sed 's/\,$//' \
+| sed -e 's/\,[[:space:]]* /\,/g')
 
-printf "%s" $query
 
-# nice BASH output (query=$(wget...))
-# echo $query | awk 'BEGIN {FS = ","}; {printf " %-30s%-30s%-30s\n", $1, $3, $5 }'
-# echo $query | awk 'BEGIN {FS = ","}; {printf "%-30s %-30s%-30s", $2, $4, $6 }'
-
+echo $query | awk 'BEGIN {FS = ","}; {printf "%-20s%-20s%-20s\n%-20s%-20s%-20s", $1, $3, $5, $2, $4, $6  }'
